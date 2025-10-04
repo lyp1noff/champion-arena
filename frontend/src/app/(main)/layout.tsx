758ce16acbfc8ca4ec88ee02b071b88ae4459c6a
lyp1noff <lyp1noff@gmail.com>
2025-10-04 21:25:37 +0100

    Update Github Actions and frontend configurations for analytics integration
    - Provided semi-dynamic Github Actions CI + docker compose ghcr
    - Modified docker-compose files to include new environment variables for analytics.
    - Updated frontend Dockerfile to accept and set analytics-related environment variables.
    - Enhanced the main layout component to conditionally load the analytics script based on the provided environment variables.

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,14 +1,22 @@
 import { Footer } from "@/components/footer";
+import Script from "next/script";
 
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const analyticsUrl = process.env.NEXT_PUBLIC_ANALYTICS_URL;
+  const analyticsId = process.env.NEXT_PUBLIC_ANALYTICS_ID;
+
   return (
     <>
       <main className="flex-1">{children}</main>
       <Footer />
+
+      {analyticsUrl && analyticsId && (
+        <Script src={analyticsUrl} data-website-id={analyticsId} strategy="afterInteractive" />
+      )}
     </>
   );
 }
