bae8ea420ad7aecb7b01c08d5bf56d076a8e802c
lyp1noff <lyp1noff@gmail.com>
2026-01-29 14:39:59 +0000

    init fix

--- backend/src/config.py
+++ backend/src/config.py
@@ -12,10 +12,7 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "champ")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-)
-DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("+asyncpg", "")
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 SERVICE_TOKEN = os.getenv("SERVICE_TOKEN", "service_token")
 JWT_SECRET = os.getenv("JWT_SECRET", "secret")
--- backend/src/models.py
+++ backend/src/models.py
@@ -15,16 +15,13 @@ from sqlalchemy import (
 )
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import (
-    DeclarativeBase,
     Mapped,
     declared_attr,
     mapped_column,
     relationship,
 )
 
-
-class Base(DeclarativeBase):
-    pass
+from .database import Base
 
 
 class Gender(enum.Enum):
